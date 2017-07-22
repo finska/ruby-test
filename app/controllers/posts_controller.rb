d9734d14e3e393423f0cb8f37b3320ba2d4bb117
Srdjan Sin Jovanovic <mp4065@gmail.com>
2017-07-23 01:19:47 +0200

    update not working yet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,33 @@ class PostsController < ApplicationController
     @user_data = User.all
   end
 
+  def show
+    @userId = User.find(params[:id])
+  end
+
   def new
 
   end
+
+  def edit
+    @userId = User.find(params[:format])
+  end
+
+  def update
+    @userId = User.find(params[:format])
+    if (@userId.update(post_params))
+      redirect_to @userId
+    else
+      render 'edit'
+    end
+  end
+
   def create
     render plain: params[:post].inspect
   end
+
+  def post_params
+    params.require(:user).permit(:address, :city, :pupcode, :puptype, :pickuptime, :municipality, :labelname)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
   get 'about' => 'pages#about'
+  get 'post/:id' => 'posts#show'
   resource :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
